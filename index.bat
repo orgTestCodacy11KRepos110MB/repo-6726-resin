e879a8055d6d1aac327dcbce688d8a07763b87fb
Kreeben <marcuslager@gmail.com>
2022-12-07 15:54:49 +0100

    respect all params

--- index.bat
+++ index.bat
@@ -1,1 +1,1 @@
-sir.bat indexwikipedia --directory C:\projects\resin\src\Sir.HttpServer\AppData\database --file d:\enwiki-20211122-cirrussearch-content.json.gz --collection wikipedia --skip 0 --take 1000 --pageSize 100 --sampleSize 1000
+sir.bat indexwikipedia --directory C:\projects\resin\src\Sir.HttpServer\AppData\database --file d:\enwiki-20211122-cirrussearch-content.json.gz --collection wikipedia --skip 0 --take 10000000 --pageSize 1000 --sampleSize 1000 %*
--- test.bat
+++ test.bat
@@ -1,3 +1,3 @@
-call sir.bat truncate --directory C:\projects\resin\src\Sir.HttpServer\AppData\database --collection wikipedia
-call index.bat
-call validate.bat
+call sir.bat truncate --directory C:\projects\resin\src\Sir.HttpServer\AppData\database --collection wikipedia %*
+call index.bat %*
+call validate.bat %*
